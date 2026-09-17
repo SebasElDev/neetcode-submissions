022951755833e993c90ff0822355721b33f22296
Sebastian Alegrett <sebasalegrett12@gmail.com>
2026-09-16 21:18:57 -0600

    Add: longest-consecutive-sequence - submission-9

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-9.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums: number[]): number {
+        if (nums.length === 0) return 0;
+
+        const numSet = new Set(nums);
+        if (numSet.size === 1) return 1;
+
+        const numSeqs = new Map<number, Set<number>>();
+        for (const num of nums) {
+            if (!numSet.has(num - 1)) {
+                numSeqs.set(num, new Set());
+                let x = num;
+                while (numSet.has(x)) {
+                    numSeqs.get(num).add(x)
+                    x++;
+                }
+            }
+
+        }
+
+        let maxLength = -Infinity;
+        for (const s of numSeqs.values()) {
+            maxLength = Math.max(maxLength, s.size);
+        }
+
+        return maxLength;
+    }
+}
