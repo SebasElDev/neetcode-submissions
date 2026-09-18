789178b179b878dd0d51572910136004527e3c38
Sebastian Alegrett <sebasalegrett12@gmail.com>
2026-09-17 19:14:08 -0600

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        const clean: string = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+        console.log(clean);
+        let l = 0, r = clean.length - 1;
+        while (l < r) {
+            if (clean[l] !== clean[r]) return false;
+            l++
+            r--
+        }
+
+        return true;
+    }
+}
